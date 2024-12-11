de5ca43968d1c21eaa69d4c5f0910aba73238032
UdhayanS <sudhayan2@gmail.com>
2024-12-11 17:19:34 +0530

    spotManagement

---
+++ src/main/java/com/example/spot/manage/spotManage/SpotManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.spot.manage.spotManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spot/manage/spotManage/SpotManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spot.manage.spotManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
