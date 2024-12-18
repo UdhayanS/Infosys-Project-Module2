2f238b204e169c059a5f998cb0e5807de16b8af9
UdhayanS <sudhayan2@gmail.com>
2024-12-18 18:33:20 +0530

    Initial commit

--- src/main/java/com/example/spot/manage/spotManage/controller/HomeController.java
+++ src/main/java/com/example/spot/manage/spotManage/controller/HomeController.java
@@ -2,14 +2,18 @@ package com.example.spot.manage.spotManage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/spotManage")
     public String home() {
         return "spotmanage";
     }
-
-   
+    
+    @PostMapping("/submitForm")
+    public String submit() {
+        return "submi";
+    }
 }
